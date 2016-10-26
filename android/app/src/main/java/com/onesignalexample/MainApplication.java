21be4f4501ec994489a5a2869b8fa59af0683a35
Spencer Carli <spencercarli@gmail.com>
2016-10-26 17:28:31 -0500

    Configure android

--- android/app/src/main/java/com/onesignalexample/MainApplication.java
+++ android/app/src/main/java/com/onesignalexample/MainApplication.java
@@ -12,6 +12,8 @@ import com.facebook.react.shell.MainReactPackage;
 import java.util.Arrays;
 import java.util.List;
 
+import com.geektime.reactnativeonesignal.ReactNativeOneSignalPackage;  // ADD THIS
+
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
@@ -23,7 +25,8 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+          new ReactNativeOneSignalPackage() // Add this line, and don't forget the comma on the previous line
       );
     }
   };
