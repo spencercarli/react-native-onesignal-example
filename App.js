c91cf86c6a7912e58a790e53a61a238ddffd572c
Spencer Carli <spencercarli@gmail.com>
2016-10-26 17:51:38 -0500

    Basic OneSignal configuration

---
+++ App.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Platform,
+} from 'react-native';
+import OneSignal from 'react-native-onesignal';
+
+class App extends Component {
+  componentDidMount() {
+    OneSignal.configure({});
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to the OneSignal Example!
+        </Text>
+        <Text style={styles.instructions}>
+          Using {Platform.OS}? Cool.
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+export default App;
